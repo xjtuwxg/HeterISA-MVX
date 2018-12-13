07ffe42349054e3de3462dedda9091c88cf9a5fa
Xiaoguang Wang <xiaoguang@vt.edu>
2018-12-13 14:46:23 -0500

    add writev print

--- inc/x86/syscallent.h
+++ inc/x86/syscallent.h
@@ -2,4 +2,5 @@
 [  1] = {3, "write", "int fd", "void *buf", "size_t count"},
 [  2] = {3, "open", "char *pathname", "int flags", "mode_t mode"},
 [  9] = {6, "mmap", "void *addr", "size_t lenght", "int prot", "int flags", "int fd", "off_t offset"},
+[ 20] = {3, "writev", "int fd", "iovec *iov", "int count"},
 [335] = {0, "endsyscall"},
