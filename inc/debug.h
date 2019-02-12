399f7e70706f2d74ffe6617cf3da78ccbc1125d4
Xiaoguang Wang <xiaoguang@vt.edu>
2019-02-12 14:03:12 -0500

    save

--- inc/debug.h
+++ inc/debug.h
@@ -45,7 +45,7 @@
  * Debug mode print message for message layer.
  * */
 //#define _MSG_DEBUG
-//#ifdef _MSG_DEBUG
+#ifdef _MSG_DEBUG
 #define MSG_PRINT(...) \
     do { \
 	fprintf(stdout, "[MVX Msg]: " __VA_ARGS__); \
